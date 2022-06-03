6fa13f61705b40e814f99f0e179b22713bfc7e1a
KatAttackTime <kaitlyn.ayer@gmail.com>
2022-06-03 09:08:35 -0400

    updated file name and added comments for clarity

--- bytes2psb.py
+++ bytes2psb.py
@@ -1,6 +1,8 @@
 import os,sys
 
-# folder = '' --- insert your folder here
+#used to change the file extension without having Microsoft PowerToys
+
+folder = '' #insert your folder here between the ''
 for filename in os.listdir(folder):
 	infilename = os.path.join(folder,filename)
 	if not os.path.isfile(infilename): continue
